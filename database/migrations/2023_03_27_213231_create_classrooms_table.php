ebe37082813144f43cba51014b1e7d4c31621050
SirenaIsmail <37363206+SirenaIsmail@users.noreply.github.com>
2023-03-28 00:34:46 +0200

    Come on

--- database/migrations/2023_03_27_213231_create_classrooms_table.php
+++ database/migrations/2023_03_27_213231_create_classrooms_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('class_rooms', function (Blueprint $table) {
+        Schema::create('classrooms', function (Blueprint $table) {
             $table->id();
             $table->integer('size');
             $table->timestamps();
@@ -27,6 +27,7 @@ return new class extends Migration
      */
     public function down()
     {
+        
         Schema::dropIfExists('class_rooms');
     }
 };
--- database/migrations/2023_03_27_213403_create_subjects_table.php
+++ database/migrations/2023_03_27_213403_create_subjects_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('sujects', function (Blueprint $table) {
+        Schema::create('subjects', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('content');
@@ -30,6 +30,7 @@ return new class extends Migration
      */
     public function down()
     {
+
         Schema::dropIfExists('sujects');
     }
 };
