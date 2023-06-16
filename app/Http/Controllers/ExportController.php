13a491b7234ba8f091202c4e11f2299e64ae8c85
SirenaIsmail <37363206+SirenaIsmail@users.noreply.github.com>
2023-06-16 17:59:23 +0300

    encrypt & decrypt

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -3,21 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Box\Spout\Common\Helper\Escaper\XLSX;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
 use Illuminate\Support\Facades\DB;
 use PDF;
 use Illuminate\Http\Request;
-use Box\Spout\Common\Type;
-use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Symfony\Component\HttpFoundation\ResponseHeaderBag;
-use Symfony\Component\HttpFoundation\StreamedResponse;
-use Illuminate\Support\Facades\Storage;
+
 class ExportController extends Controller
 {
-    public function exportExcel()
+
+    public function encryptExcel(Request $request)
     {
+        // يجب تعديله بحيث يأخذ المدربين الأكثر تقييماً
         // Get the users data from the database
         $users = DB::table('users')->select('users.first_name', 'users.last_name', 'users.branch_id', 'users.phone_number', 'users.email')->get();
+        // Get the encryption key from the input field
+        $encryptionKey = $request->input('encryption_key');
 
         // Create a new Excel writer
         $writer = WriterEntityFactory::createXLSXWriter();
@@ -32,14 +34,24 @@ class ExportController extends Controller
         ]);
         $writer->addRow($headerRow);
 
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('AES-256-CBC'));
+
         // Write the data rows
         foreach ($users as $user) {
+            // Encode each value using a password
+            $encryptedFirstName = openssl_encrypt($user->first_name, 'AES-256-CBC', $encryptionKey, 0, 'my_secret_iv');
+            $encryptedLastName = openssl_encrypt($user->last_name, 'AES-256-CBC', $encryptionKey, 0, 'my_secret_iv');
+            $encryptedBranchId = openssl_encrypt($user->branch_id, 'AES-256-CBC', $encryptionKey, 0, 'my_secret_iv');
+            $encryptedPhoneNumber = openssl_encrypt($user->phone_number, 'AES-256-CBC', $encryptionKey, 0, 'my_secret_iv');
+            $encryptedEmail = openssl_encrypt($user->email, 'AES-256-CBC', $encryptionKey, 0, 'my_secret_iv');
+
+            // Create a row with the encrypted values
             $row = WriterEntityFactory::createRowFromArray([
-                $user->first_name,
-                $user->last_name,
-                $user->branch_id,
-                $user->phone_number,
-                $user->email,
+                $encryptedFirstName,
+                $encryptedLastName,
+                $encryptedBranchId,
+                $encryptedPhoneNumber,
+                $encryptedEmail,
             ]);
             $writer->addRow($row);
         }
@@ -49,7 +61,50 @@ class ExportController extends Controller
 
         // Return a response to indicate that the file has been saved
         return response()->download($filePath, 'users.xlsx')->deleteFileAfterSend(true);
-
     }
+
+
+    public function decryptExcel(Request $request)
+    {
+        // Get the file path of the uploaded file
+        $filePath = $request->file('excel_file')->getPathname();
+
+        // Get the decryption key from the input field
+        $decryptionKey = $request->input('decryption_key');
+
+        // Create a new Excel reader
+        $reader = ReaderEntityFactory::createXLSXReader();
+
+        // Open the file for reading
+        $reader->open($filePath);
+
+        // Iterate over each row in the file
+        foreach ($reader->getSheetIterator() as $sheet) {
+            foreach ($sheet->getRowIterator() as $row) {
+                // Decrypt each value using the decryption key
+                $decryptedFirstName = openssl_decrypt($row[0], 'AES-256-CBC', $decryptionKey, 0, 'my_secret_iv');
+                $decryptedLastName = openssl_decrypt($row[1], 'AES-256-CBC', $decryptionKey, 0, 'my_secret_iv');
+                $decryptedBranchId = openssl_decrypt($row[2], 'AES-256-CBC', $decryptionKey, 0, 'my_secret_iv');
+                $decryptedPhoneNumber = openssl_decrypt($row[3], 'AES-256-CBC', $decryptionKey, 0, 'my_secret_iv');
+                $decryptedEmail = openssl_decrypt($row[4], 'AES-256-CBC', $decryptionKey, 0, 'my_secret_iv');
+
+                // Create a new row with the decrypted values
+                $newRow = [
+                    $decryptedFirstName,
+                    $decryptedLastName,
+                    $decryptedBranchId,
+                    $decryptedPhoneNumber,
+                    $decryptedEmail,
+                ];
+
+                // Print the decrypted values to the console
+                print_r($newRow);
+            }
+        }
+
+        // Close the reader to release the file lock
+        $reader->close();
+    }
+
 
 }
