db7cbb67f629693a5c0fe74e3d3c6147bf1decdb
HaninKazour <118918314+HaninKazour@users.noreply.github.com>
2023-06-20 11:03:21 +0300

    sub

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -79,7 +79,7 @@ class SubscribeController extends Controller
             'card_id'=>  $request->card_id,
             'branch_id'=> $branchId,
             'state'=>  $state,
-            'date_id'=>$request->date_id,
+        
         ]);
 
         if($dataSubscribe)
@@ -233,6 +233,7 @@ class SubscribeController extends Controller
      */
     public function update(Request $request,$id)
     {
+
         $dataSubscribe = Subscribe::find($id);
 
         if(!$dataSubscribe)
@@ -245,6 +246,7 @@ class SubscribeController extends Controller
             'state'=> 'required',
 
         ]);
+
         if($validation->fails())
 
         {
@@ -289,6 +291,7 @@ class SubscribeController extends Controller
 
 
     public function search($filter)
+    
     {
         if (auth()->check()) {
             $branchId = Auth::user()->branch_id;
@@ -305,24 +308,26 @@ class SubscribeController extends Controller
                 ->select('subscribes.state','subjects.subjectName','subjects.content', 'subjects.price' ,'cards.barcode', 'users.first_name', 'users.last_name', 'users.phone_number')
                 ->where('user_branch.id', '=', $branchId) // تحديد فقط الاشتراكات في فرع المستخدم
                 ->where(function ($query) use ($filter) { // التحقق من وجود نتائج بعد تطبيق الفلتر
-                    $query->where('subscribes.state', 'like', "%$filter%");
+                    $query->where('subscribes.state', 'like', "%$filter%")
+                           ->orWhere('users.first_name', 'like', "%$filter%");
                 })
-                ->paginate(10);
+                   ->paginate(10);
             }
-            else{
+
+            else {
                 
 
-            $filterResult = DB::table('subscribes')
-            ->join('subjects', 'subscribes.subject_id', '=', 'subjects.id')
+          $filterResult = DB::table('subscribes')
+            ->join('courses', 'subscribes.course_id', '=', 'courses.id')
+            ->join('subjects', 'courses.subject_id', '=', 'subjects.id')
             ->join('cards', 'subscribes.card_id', '=', 'cards.id')
             ->join('branches as card_branch', 'cards.branch_id', '=', 'card_branch.id')
             ->join('users', 'cards.user_id', '=', 'users.id')
             ->join('branches as user_branch', 'users.branch_id', '=', 'user_branch.id')
-            // 'subjects.subjectName', 
-            ->select('subscribes.state','subjects.subjectName','subjects.content', 'subjects.price' ,'cards.barcode', 'users.first_name', 'users.last_name', 'users.phone_number')
-            ->where('user_branch.id', '=', $branchId) // تحديد فقط الاشتراكات في فرع المستخدم
-            
-            ->paginate(10);
+                // 'subjects.subjectName', 
+                ->select('subscribes.state','subjects.subjectName','subjects.content', 'subjects.price' ,'cards.barcode', 'users.first_name', 'users.last_name', 'users.phone_number')
+                ->where('user_branch.id', '=', $branchId) // تحديد فقط الاشتراكات في فرع المستخدم
+                ->paginate(10);
             }
             if ($filterResult->count() > 0) {
                 return $this->traitResponse($filterResult, 'Search Successfully', 200);
@@ -334,4 +339,37 @@ class SubscribeController extends Controller
         }
         
     }
+
+    
+
+    public function searchDate($filter)
+    
+    {
+        if (auth()->check()) {
+            $branchId = Auth::user()->branch_id;
+    
+            $filterResult = DB::table('subscribes')
+            ->join('courses', 'subscribes.course_id', '=', 'courses.id')
+            ->join('subjects', 'courses.subject_id', '=', 'subjects.id')
+            ->join('cards', 'subscribes.card_id', '=', 'cards.id')
+            ->join('branches as card_branch', 'cards.branch_id', '=', 'card_branch.id')
+            ->join('users', 'cards.user_id', '=', 'users.id')
+            ->join('branches as user_branch', 'users.branch_id', '=', 'user_branch.id') 
+                ->select('subscribes.state','subjects.subjectName','subjects.content', 'subjects.price' ,'cards.barcode', 'users.first_name', 'users.last_name', 'users.phone_number')
+                ->where('user_branch.id', '=', $branchId) // تحديد فقط الاشتراكات في فرع المستخدم
+                ->where(function ($query) use ($filter) { // التحقق من وجود نتائج بعد تطبيق الفلتر
+                    $query->where('subscribes.state', 'like', "%$filter%")
+                           ->orWhere('users.first_name', 'like', "%$filter%");
+                })
+                   ->paginate(10);
+            
+
+            if ($filterResult->count() > 0) {
+                return $this->traitResponse($filterResult, 'Search Successfully', 200);
+            } else {
+                return $this->traitResponse(null, 'No matching results found', 200);
+            }
+         } else {
+            return $this->traitResponse(null, 'User not authenticated', 401);
+        }
 }
